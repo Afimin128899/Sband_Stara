7a8d9040ab29a978c89504eacba7deb973718e37
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:57:33 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -5,41 +5,15 @@ def main_menu():
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(
-                    text="👤 Профиль",
-                    callback_data="profile"
-                ),
-                InlineKeyboardButton(
-                    text="📋 Задания",
-                    callback_data="tasks"
-                ),
+                InlineKeyboardButton("📋 Задания", callback_data="tasks"),
+                InlineKeyboardButton("💸 Вывод", callback_data="withdraw_menu"),
             ],
             [
                 InlineKeyboardButton(
-                    text="🛠 Тех. поддержка",
+                    "🛠 Поддержка",
                     url="https://t.me/ShardenFoot"
                 )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="15 ⭐",
-                    callback_data="exchange_15"
-                ),
-                InlineKeyboardButton(
-                    text="25 ⭐",
-                    callback_data="exchange_25"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="50 ⭐",
-                    callback_data="exchange_50"
-                ),
-                InlineKeyboardButton(
-                    text="100 ⭐",
-                    callback_data="exchange_100"
-                ),
-            ],
+            ]
         ]
     )
     
