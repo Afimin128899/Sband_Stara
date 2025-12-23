597d6adccde27d221d48b7d816744659ae374640
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-24 00:17:24 +0300

    Update users.py

--- utils/users.py
+++ utils/users.py
@@ -1,11 +1,17 @@
 from utils.storage import users
 
-
 def get_user(user_id: int):
     if user_id not in users:
         users[user_id] = {
-            "stars": 0,
-            "referrals": 0
+            "stars": 0,          # баланс
+            "referrals": 0,      # количество приглашённых
+            "withdrawn": 0       # сколько всего выведено
         }
     return users[user_id]
+
+def add_referral(user_id: int):
+    """Добавить 1 приглашённого и +2 звезды"""
+    user = get_user(user_id)
+    user["referrals"] += 1
+    user["stars"] += 2
     
