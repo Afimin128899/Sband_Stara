5f435d46a5b86d36338f2f612badb61c654e1cbe
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:01:37 +0300

    Update subgram_api.py

--- utils/subgram_api.py
+++ utils/subgram_api.py
@@ -1,7 +1,7 @@
 import requests
 
 SUBGRAM_API = "https://api.subgram.org/api/v1"
-SUBGRAM_API_KEY = "ВАШ_SUBGRAM_API_KEY"
+SUBGRAM_API_KEY = "de2e0cfbc0589fa6e3fac80da0403dc532e3c8d96bd97e1fd314c7f92fd0af03"
 
 def get_sponsors(user_id):
     """Получение заданий (спонсоров) через SubGram API"""
