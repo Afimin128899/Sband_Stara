f56205cb9e66f418c87c3881d3a275b6a226dbdb
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:01:15 +0300

    Update users.py

--- utils/users.py
+++ utils/users.py
@@ -1,1 +1,12 @@
+# Хранилище пользователей (для простоты, в реальной версии - база)
+users = {}
 
+def get_user(user_id):
+    if user_id not in users:
+        users[user_id] = {
+            "stars": 0,
+            "referrals": 0,
+            "ref_earnings": 0,
+            "ref_link": f"https://t.me/YourBot?start={user_id}"
+        }
+    return users[user_id]
