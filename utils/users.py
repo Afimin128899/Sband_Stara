7148c6f604d9de3de1b704a7500f1db3ec8404f0
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:49:07 +0300

    Update users.py

--- utils/users.py
+++ utils/users.py
@@ -7,6 +7,6 @@ def get_user(user_id):
             "stars": 0,
             "referrals": 0,
             "ref_earnings": 0,
-            "ref_link": f"https://t.me/YourBot?start={user_id}"
+            "ref_link": f"https://t.me/Sband_Stars_Bot?start={user_id}"
         }
     return users[user_id]
