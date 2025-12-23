66e480152415918a0a7498425f1753e9bd70d295
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:00:29 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,1 +1,16 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def main_menu():
+    markup = InlineKeyboardMarkup(row_width=2)
+    markup.add(
+        InlineKeyboardButton("Профиль", callback_data="profile"),
+        InlineKeyboardButton("Задания", callback_data="tasks"),
+        InlineKeyboardButton("Тех. поддержка", url="https://t.me/ShardenFoot")
+    )
+    markup.add(
+        InlineKeyboardButton("15 звёзд", callback_data="exchange_15"),
+        InlineKeyboardButton("25 звёзд", callback_data="exchange_25"),
+        InlineKeyboardButton("50 звёзд", callback_data="exchange_50"),
+        InlineKeyboardButton("100 звёзд", callback_data="exchange_100")
+    )
+    return markup
