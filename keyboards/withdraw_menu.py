ab8d0711902ae2ef2a7135e159379d89789b7f15
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:57:48 +0300

    Update withdraw_menu.py

--- keyboards/withdraw_menu.py
+++ keyboards/withdraw_menu.py
@@ -1,1 +1,20 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+def withdraw_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton("15 ⭐", callback_data="withdraw_15"),
+                InlineKeyboardButton("25 ⭐", callback_data="withdraw_25"),
+            ],
+            [
+                InlineKeyboardButton("50 ⭐", callback_data="withdraw_50"),
+                InlineKeyboardButton("100 ⭐", callback_data="withdraw_100"),
+            ],
+            [
+                InlineKeyboardButton("🔙 Назад", callback_data="back_main"),
+            ],
+        ]
+    )
+  
