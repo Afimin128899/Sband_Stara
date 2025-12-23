55f2e2f44329b43fd6fce4df47840a04309a85f7
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-24 00:29:24 +0300

    Update profile_menu.py

--- keyboards/profile_menu.py
+++ keyboards/profile_menu.py
@@ -1,1 +1,15 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def profile_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text="📊 Профиль", callback_data="show_profile"),
+                InlineKeyboardButton(text="👥 Реферальная система", callback_data="referrals"),
+            ],
+            [
+                InlineKeyboardButton(text="🔙 Назад", callback_data="back_main")
+            ]
+        ]
+    )
+  
