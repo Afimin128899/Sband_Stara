67a663eb24ba558cfac75cd424fc43b86ba1555c
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 23:57:06 +0300

    Update flyer_api.py

--- utils/flyer_api.py
+++ utils/flyer_api.py
@@ -1,1 +1,23 @@
+import requests
 
+API_KEY = "FL-JCQcno-ZEliXE-fQqxRr-rfbkQS"
+API_URL = "https://api.flyerservice.io"
+
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
+
+
+def get_tasks(user_id: int):
+    try:
+        r = requests.get(
+            f"{API_URL}/v1/tasks",
+            headers=HEADERS,
+            params={"user_id": user_id},
+            timeout=10
+        )
+        return r.json().get("tasks", [])
+    except:
+        return []
+      
