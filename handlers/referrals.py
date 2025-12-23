031a6f91db6540eabe23845b5993c2f612912026
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-23 22:59:06 +0300

    Update referrals.py

--- handlers/referrals.py
+++ handlers/referrals.py
@@ -1,1 +1,7 @@
+from utils.users import get_user
 
+def handle_referral(user_id, ref_id):
+    if ref_id != user_id:
+        ref_user = get_user(ref_id)
+        ref_user["referrals"] += 1
+        ref_user["ref_earnings"] += 2  # 2 звезды за приглашение
