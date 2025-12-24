5b43b7470788055f724dcb8367f3abfdf07af210
Afimin128899 <140860471+Afimin128899@users.noreply.github.com>
2025-12-24 08:59:11 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,18 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def main_menu():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(text="📊 Профиль", callback_data="show_profile"),
-                InlineKeyboardButton(text="📋 Задания", callback_data="tasks"),
-            ],
-            [
-                InlineKeyboardButton(text="💸 Вывод", callback_data="withdraw_menu"),
-            ],
-            [
-                InlineKeyboardButton(text="🛠 Поддержка", url="https://t.me/ShardenFoot")
-            ]
-        ]
-    )
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📋 Задания", callback_data="tasks")],
+        [InlineKeyboardButton(text="👤 Профиль", callback_data="profile")],
+        [InlineKeyboardButton(text="💸 Вывод", callback_data="withdraw")]
+    ])
     
